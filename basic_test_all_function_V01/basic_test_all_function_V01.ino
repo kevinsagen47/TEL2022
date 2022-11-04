a2081caa1b4208812ba7bcfa2fe8b819baae0b6a
kevinsagen47 <kevinsagen47@gmail.com>
2022-11-04 20:44:59 +0800

    create test all function, stable rc_v01

---
+++ basic_test_all_function_V01/basic_test_all_function_V01.ino
@@ -0,0 +1,154 @@
+#include <PIDController.h>
+volatile long int encoder_pos = 0;
+volatile long int encoder_pos2 = 0;
+volatile long int encoder_pos3 = 0;
+volatile long int encoder_pos4 = 0;
+int32_t measured_time, measured_time_start;
+uint8_t channel_select_counter;
+PIDController pos_pid; 
+int motor_value = 255;
+unsigned int integerValue=0;  // Max value is 65535
+char incomingByte;
+#include <PPMReader.h>
+byte interruptPin = 2;
+byte channelAmount = 8;
+PPMReader ppm(interruptPin, channelAmount);
+int channel_[10]={0,1,2,3,4,5,6,7,8,9};
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(18, INPUT);
+  pinMode(14, INPUT);
+  
+  pinMode(19, INPUT);
+  pinMode(15, INPUT);
+  
+  pinMode(20, INPUT);
+  pinMode(16, INPUT);
+  
+  pinMode(21, INPUT);
+  pinMode(17, INPUT);
+  
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(18), encoder, RISING);
+  attachInterrupt(digitalPinToInterrupt(19), encoder2, RISING);
+  attachInterrupt(digitalPinToInterrupt(20), encoder3, RISING);
+  attachInterrupt(digitalPinToInterrupt(21), encoder4, RISING);
+  
+
+  pos_pid.begin();    
+  pos_pid.tune(15, 0, 2000);    
+  pos_pid.limit(-255, 255);
+  delay(100);
+}
+
+void loop() {
+for (byte channel = 1; channel <= channelAmount; ++channel) {
+        unsigned value = ppm.latestValidChannelValue(channel, 0);
+        //Serial.print(value);
+        channel_[channel]=value;
+        if(channel < channelAmount) Serial.print('\t');
+    }
+    //Serial.println();
+
+/*  
+if (Serial.available() > 0) {  
+    integerValue = 0;        
+    while(1) {           
+      incomingByte = Serial.read();
+      if (incomingByte == '\n') break;   
+      if (incomingByte == -1) continue;  
+      integerValue *= 10;  
+      integerValue = ((incomingByte - 48) + integerValue);
+      pos_pid.setpoint(integerValue);
+    }
+}
+*/
+   motor_value = pos_pid.compute(encoder_pos);
+if(motor_value > 0){
+  MotorCounterClockwise(motor_value);
+}else{
+  MotorClockwise(abs(motor_value));
+}
+  Serial.print("Enc: ");
+  Serial.print(encoder_pos);
+  Serial.print("  ");
+  Serial.print(encoder_pos2);
+  Serial.print("  ");
+  Serial.print(encoder_pos3);
+  Serial.print("  ");
+  Serial.print(encoder_pos4);
+  Serial.print("  ppm: ");
+  Serial.print(channel_[1]);
+  Serial.print("  ");
+  Serial.print(channel_[2]);
+  Serial.print("  ");
+  Serial.print(channel_[3]);
+  Serial.print("  ");
+  Serial.print(channel_[4]);
+  Serial.print("  ");
+  Serial.print(channel_[5]);
+  Serial.print("  ");
+  Serial.print(channel_[6]);
+  Serial.print("  ");
+  Serial.print(channel_[7]);
+  Serial.println();
+  delay(100);
+}
+
+/*
+  pinMode(PB12, INPUT);
+  pinMode(PB13, INPUT);
+  
+  pinMode(PB14, INPUT);
+  pinMode(PB15, INPUT);
+  
+  pinMode(PA15, INPUT);
+  pinMode(PB3, INPUT);
+  
+  pinMode(PB4, INPUT);
+  pinMode(PB5, INPUT);
+  
+  pinMode(PB6, OUTPUT);
+  pinMode(PB7, OUTPUT);
+*/
+void encoder4(){
+  if(digitalRead(17) == HIGH)encoder_pos4++;
+  else encoder_pos4--;}
+
+
+void encoder3(){
+  if(digitalRead(16) == HIGH)encoder_pos3++;
+  else encoder_pos3--;}
+
+
+void encoder2(){
+  if(digitalRead(15) == HIGH)encoder_pos2++;
+  else encoder_pos2--;}
+
+void encoder(){
+  if(digitalRead(14) == HIGH)encoder_pos--;
+  else encoder_pos++;}
+
+void MotorClockwise(int power){
+  if(power > 100){
+  analogWrite(9, power);
+  digitalWrite(10, LOW);
+  }else{
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+  }
+}
+
+void MotorCounterClockwise(int power){
+  if(power > 100){
+  analogWrite(10, power);
+  digitalWrite(9, LOW);
+  }else{
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+  }
+}
--- rc_V01/rc_V01.ino
+++ rc_V01/rc_V01.ino
@@ -14,7 +14,7 @@ byte interruptPin = 2;
 byte channelAmount = 8;
 PPMReader ppm(interruptPin, channelAmount);
 int channel_[10]={0,1,2,3,4,5,6,7,8,9};
-int forward,backward,left,right,turn_left,turn_right;
+int forward,side,turn;
 int f_r,f_l,b_r,b_l;
 int _f_r,_f_l,_b_r,_b_l;
 //1: x_r
@@ -65,61 +65,31 @@ for (byte channel = 1; channel <= channelAmount; ++channel) {
     }
 
   if(channel_[2]>50){
-    if(channel_[2]>1549){
-      forward=map(channel_[2],1550,2000,0,255);
-      backward=0;} 
-    else if (channel_[2]<1450)  {
-      backward=map(channel_[2],1450,1000,0,255);
-      forward=0;}
-    else {
-      forward=0;
-      backward=0;}
+    if(channel_[2]>1549)forward=map(channel_[2],1550,2000,0,255);
+    else if (channel_[2]<1450) forward=-1*map(channel_[2],1450,1000,0,255);
+    else forward=0;
 
     if (channel_[1]>50){
-      if(channel_[1]>1549){
-        right=map(channel_[1],1550,2000,0,255);
-        left=0;} 
-      else if (channel_[1]<1450){
-        left=map(channel_[1],1450,1000,0,255);
-        right=0;}
-      else {
-        right=0;
-        left=0;}
-    }
+      if(channel_[1]>1549)side=map(channel_[1],1550,2000,0,255);
+      else if (channel_[1]<1450)side=-1*map(channel_[1],1450,1000,0,255);
+      else side=0;}
 
     if (channel_[4]>50){
-      if(channel_[4]>1549){
-        turn_right=map(channel_[4],1550,2000,0,255);
-        turn_left=0;} 
-      else if (channel_[4]<1450){
-        turn_left=map(channel_[4],1450,1000,0,255);
-        turn_right=0;}
-      else {
-        turn_right=0;
-        turn_left=0;}
-    }
+      if(channel_[4]>1549)turn=map(channel_[4],1550,2000,0,255);
+      else if (channel_[4]<1450)turn=-1*map(channel_[4],1450,1000,0,255);
+      else turn=0;}
     
     //backward=0;
-    f_r=forward+right+turn_left;
-    f_l=forward+left+turn_right;
-    b_r=forward+left+turn_left;
-    b_l=forward+right+turn_right;
-
-    _f_r=backward+left+turn_right;
-    _f_l=backward+right+turn_left;
-    _b_r=backward+right+turn_right;
-    _b_l=backward+left+turn_left;
+    f_r=   forward+   side+-1*turn;
+    f_l=   forward+-1*side+   turn;
+    b_r=forward+-1*side+-1*turn;
+    b_l=forward+   side+   turn;
   }
   else{
     f_r=0;
     f_l=0;
     b_r=0;
-    b_l=0;
-
-    _f_r=0;
-    _f_l=0;
-    _b_r=0;
-    _b_l=0;    
+    b_l=0;   
   }
 
   //Serial.print(forward);
@@ -127,15 +97,37 @@ for (byte channel = 1; channel <= channelAmount; ++channel) {
   //Serial.print(backward);
   Serial.print(channel_[4]);
   Serial.println();
-  analogWrite(13,f_r);
-  analogWrite(11,f_l);
-  analogWrite(9,b_r);
-  analogWrite(7,b_l);
+  
+  
+  
 
-  if(f_r==0)analogWrite(12,_f_r);
-  if(f_l==0)analogWrite(10,_f_l);
-  if(b_r==0)analogWrite(8,_b_r);
-  if(b_l==0)analogWrite(6,_b_l);
+  if(f_r>=0){
+    analogWrite(13,f_r);
+    analogWrite(12,0);}
+  else {
+    analogWrite(12,-1*f_r);
+    analogWrite(13,0);}
+    
+  if(f_l>=0){
+    analogWrite(11,f_l);
+    analogWrite(10,0);}
+  else {
+    analogWrite(10,-1*f_l);
+    analogWrite(11,0);}
+  
+  if(b_r>=0){
+    analogWrite(9,b_r);
+    analogWrite(8,0);}
+  else {
+    analogWrite(8,-1*b_r);
+    analogWrite(9,0);}
+
+  if(b_l>=0){
+    analogWrite(7,b_l);
+    analogWrite(6,0);}
+  else {
+    analogWrite(6,-1*b_l);
+    analogWrite(7,0);}
 
 }
 
