cc5ed0bee4811e6ed80acb762e9e83d25973e155
JhengLun <118558858+JhengLun@users.noreply.github.com>
2022-11-27 00:39:31 +0800

    fast speed 220 level2

--- basic_test_all_function_V02/basic_test_all_function_V02.ino
+++ basic_test_all_function_V02/basic_test_all_function_V02.ino
@@ -111,15 +111,15 @@ void loop() {
   delay(200);
   while(digitalRead(42))run(); 
   
-  forward_degree(120,1100);wait(500);//出發前進第二關20
+  forward_degree(220,1100);wait(500);//出發前進第二關20
   cal_left(360);wait(1000);//靠左矯正-到第一個點
-  forward_degree(120,800);wait(500);//出發前進第二關40
-  side_degree(120,-410);wait(1000);//左移
-  forward_degree(120,800);wait(500);//前進
+  forward_degree(220,800);wait(500);//出發前進第二關40
+  side_degree(220,-440);wait(1000);//左移
+  forward_degree(220,800);wait(500);//前進
   cal_right(310);wait(1000);//靠右矯正-到第二個點
-  forward_degree(120,700);wait(1000);//出發前進第二關60
-  side_degree(120,430);wait(500);//右移
-  forward_degree(120,660);wait(500);//前進到第二關60
+  forward_degree(220,700);wait(1000);//出發前進第二關60
+  side_degree(220,430);wait(500);//右移
+  forward_degree(180,660);wait(500);//前進到第二關60
   cal_left(310);wait(5000);//靠右矯正-到第三個點
  
 
